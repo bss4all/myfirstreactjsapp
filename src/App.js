5906721162da15cb7305d5b985ba9f0b5f0cdf51
bss4all <bss4all@gmail.com>
2025-07-17 18:03:01 +0530

    selectoption chagnes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [selectedFruit, setSelectedFruit] = useState('apple');
+  const [selectedColor, setSelectedColor] = useState('red');
+
+  const handleChange = (event) => {
+    setSelectedFruit(event.target.value);
+  };
+
+  const handleColor = (event) => {
+    setSelectedColor(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="fruit-select">Choose a fruit:</label>
+      <select id="fruit-select" value={selectedFruit} onChange={handleChange}>
+        <option value="apple">Apple</option>
+        <option value="banana">Banana</option>
+        <option value="orange">Orange</option>
+      </select>
+      <p>You selected: {selectedColor}</p>
+      <label htmlFor="color-select">Choose a Color:</label>
+      <select id="color-select" value={selectedFruit} onChange={handleColor}>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="green">Green</option>
+      </select>
+      <p>You selected Color : {selectedColor}</p>
     </div>
   );
 }
